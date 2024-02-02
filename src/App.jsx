96bef2ba214131a2bbc7d9b50a236b44ba8fe5fb
akashagrawal9179 <akashagrawal5012@gmail.com>
2024-02-02 15:32:12 +0530

    #Added base setup of vite.

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Homepage from "./Components/Homepage";
+import CartItem from "./Components/CartItem";
+import { Provider } from "react-redux";
+import store from "./Store";
+
+const App = () => {
+  return (
+    <>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/cart-item" element={<CartItem />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    </>
+  );
+};
+
+export default App;
